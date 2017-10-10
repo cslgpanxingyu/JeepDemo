411ce8f085cf08a831bdbf2bf56c11ff4cfec3b1
潘星宇 <pxy14077@ly.com>
2017-10-10 17:41:23 +0800

    初始化

---
+++ JeepGosh/JeepGosh/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JeepModel;
+
+namespace JeepGosh.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static List<Person> Persons = new List<Person>(){
+            new Person{User="潘星宇",UserId=1,Number="14077",PhoneNumber="18862482729",Department="财务管理开发组"},
+            new Person{User="欧阳",UserId=2,Number="17705",PhoneNumber="1555222557",Department="财务管理开发组"},
+            new Person{User="XXX",UserId=3,Number="35852",PhoneNumber="855447411",Department="财务管理开发组"},
+            new Person{User="大黄",UserId=4,Number="95585",PhoneNumber="258741225",Department="财务管理开发组"},
+        };
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetList()
+        {
+            return new JsonResult
+            {
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet,
+                Data = new { Users = Persons }
+            };
+        }
+
+        public ActionResult DoSomething(Person person)
+        {
+            var result = Persons.SingleOrDefault(u => u.UserId == person.UserId);
+            if (result == null)
+            {
+                return new JsonResult { Data = new { Success = false, Message = "没有该用户" }, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+            else
+            {
+                return new JsonResult
+                {
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet,
+                    Data = new { Success = true, Data = result }
+                };
+            }
+        }
+    }
+}
---
+++ JeepGosh/JeepModel/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeepModel
+{
+    public class Person
+    {
+        public string User { get; set; }
+
+        public int UserId { get; set; }
+
+        public string Number { get; set; }
+
+        public string Department { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
